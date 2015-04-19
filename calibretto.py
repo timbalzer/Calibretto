8f56e7ff48f8925f770b43f667f9da55b27c1517
Tim Balzer <balzer.tim@gmail.com>
2015-04-19 19:25:21 -0400

    Rebuild Calibretto for Python 3.x with Peewee

--- calibretto.py
+++ calibretto.py
@@ -1,101 +1,159 @@
-#!/usr/bin/env /usr/local/bin/python2.7
+#  Calibretto 1.1
+#  Calibretto allows you to choose keywords to track on
+#  Twitter and store associated tweets in your database.
 
-import twitter
-import psycopg2
 import sys
-
-# Twitter authentication
-# Grab your creds at https://apps.twitter.com
-
-CONSUMER_KEY = '{Consumer Key (API Key)}'
-CONSUMER_SECRET = '{Consumer Secret (API Secret)}'
-OAUTH_TOKEN = '{Access Token}'
-OAUTH_TOKEN_SECRET = '{Access Token Secret}'
-
-auth = twitter.oauth.OAuth(OAUTH_TOKEN, OAUTH_TOKEN_SECRET, CONSUMER_KEY, CONSUMER_SECRET)
-
-twitter_api = twitter.Twitter(domain='api.twitter.com', api_version='1.1', auth=auth)
-
-# Connect to your PostgreSQL database
-conn = psycopg2.connect(database="{database}", host="{host}", port="{port}", user="{username}", password="{password}")
-cur = conn.cursor()
-
-# Choose keywords
-harvest_list = ['{keyword1}','{keyword2}']
-
-# Create BatchID
-cur.execute("select max(coalesce(batchid,0)) from tweetlog")
-batch_id_cur = cur.fetchall()
-if batch_id_cur[0][0] is None:
-    batch_id = 0
-else:
-    batch_id = batch_id_cur[0][0]+1
+import twitter
+from peewee import *
 
 
-# Given a keyword, perform harvest
-for tweet_keyword in harvest_list: 
+#  Twitter authentication
+#  Grab your creds at https://apps.twitter.com
+CONSUMER_KEY = "{Consumer Key (API Key)}"
+CONSUMER_SECRET = "{Consumer Secret (API Secret)}"
+OAUTH_TOKEN = "{Access Token}"
+OAUTH_TOKEN_SECRET = "{Access Token Secret}"
 
-        # Truncate temp table
-        cur.execute("""truncate temptweets""")
-        conn.commit()
+auth = twitter.oauth.OAuth(OAUTH_TOKEN,
+                           OAUTH_TOKEN_SECRET,
+                           CONSUMER_KEY,
+                           CONSUMER_SECRET)
 
-        # Perform search with given criteria
-        search_results = twitter_api.search.tweets(q=tweet_keyword, count=100)
+twitter_api = twitter.Twitter(domain="api.twitter.com",
+                              api_version="1.1",
+                              auth=auth)
 
-        # Set up each tweet as a status
-        for status in search_results['statuses']:
-            
-            # Only return information for tweets with geo info
+
+#  Connect to your PostgreSQL database
+db = PostgresqlDatabase("{database}",
+                        host="{host}",
+                        port="{port}",
+                        user="{username}",
+                        password="{password}")
+
+
+#  Choose keywords
+harvest_list = ["{keyword1}","{keyword2}"]
+
+
+#  Set up data classes
+class BaseModel(Model):
+    class Meta:
+        database = db
+
+
+class Temptweets(BaseModel):
+    tweet_id = PrimaryKeyField()
+    tweet_datetime = DateTimeField()
+    tweet_keyword = CharField(max_length=50)
+    tweet  = CharField(max_length=200)
+    tweeter = CharField(max_length=50)
+    lang = CharField(max_length=50)
+    latitude = DoubleField()
+    longitude = DoubleField()
+
+
+class Tweets(BaseModel):
+    tweet_id = PrimaryKeyField()
+    tweet_datetime = DateTimeField()
+    tweet_keyword = CharField(max_length=50)
+    tweet  = CharField(max_length=200)
+    tweeter = CharField(max_length=50)
+    lang = CharField(max_length=50)
+    latitude = DoubleField()
+    longitude = DoubleField()
+
+
+class Tweetlog(BaseModel):
+    runid = PrimaryKeyField()
+    batchid = IntegerField()
+    rundate = DateTimeField()
+    keyword = CharField(max_length=50)
+    harvestedthisrun = IntegerField()
+    totalharvested = IntegerField()
+
+
+#  Set up app functions
+def initialize():
+    db.connect()
+
+
+def get_tweets():
+    for tweet_keyword in harvest_list:
+        search_results = twitter_api.search.tweets(q=tweet_keyword,count=100)
+        for status in search_results["statuses"]:
             if status["geo"]:
-
-                # Create variables
-                tweet_id = status['id_str']
+                tweet_id = status["id_str"]
                 tweet_datetime = status["created_at"]
                 tweet = status["text"]
-                tweeter = "@" + status['user']['screen_name']
-                lang = status['metadata']['iso_language_code']
+                tweeter = "@" + status["user"]["screen_name"]
+                lang = status["metadata"]["iso_language_code"]
                 latitude = status["geo"]["coordinates"][0]
                 longitude = status["geo"]["coordinates"][1]
 
-                # Print a nice readout of each tweet
-                print "     tweet_id: ",tweet_id,"\n"
-                print "     tweet_datetime: ",tweet_datetime,"\n"
-                print "     tweet_keyword: ",tweet_keyword,"\n"
-                print "     tweet: ",tweet.encode('utf-8'),"\n"
-                print "     tweeter: ",tweeter,"\n"
-                print "     lang: ",lang,"\n"
-                print "     latitude: ",latitude,"\n"
-                print "     longitude: ",longitude,"\n"
-                print "     #######################","\n"
+                print("\n##\n"
+                      "tweet_id: {}\n"
+                      "tweet_datetime: {}\n"
+                      "tweet_keyword: {}\n"
+                      "tweet: {}\n"
+                      "tweeter: {}\n"
+                      "lang: {}\n"
+                      "latitude: {}\n"
+                      "longitude: {}\n"
+                      "##".format(tweet_id,
+                                  tweet_datetime,
+                                  tweet_keyword,
+                                  tweet,
+                                  tweeter,
+                                  lang,
+                                  latitude,
+                                  longitude))
 
-                # Attempt to place tweets into temp table
-                try:    
-                    cur.execute("""insert into temptweets (tweet_id, tweet_datetime, tweet_keyword, tweet, tweeter, lang, latitude, longitude)
-                                values (%s, %s, %s, %s, %s, %s, %s, %s);""",
-                                (tweet_id, tweet_datetime, tweet_keyword, tweet.encode('utf-8'), tweeter, lang, latitude, longitude)
-								) 
-                # If there is an error, print the error type and skip tweet
+                try:
+                    Temptweets.insert(tweet_id=tweet_id,
+                                      tweet_datetime=tweet_datetime,
+                                      tweet_keyword=tweet_keyword,
+                                      tweet=tweet, tweeter=tweeter,
+                                      lang=lang, latitude=latitude,
+                                      longitude=longitude).execute()
                 except:
-                    print "############### Error:", sys.exc_info()[0], "###############"
+                    print("## Error: {} ##".format(sys.exc_info()))
+    copy_tweets()
 
-        # Move new tweets into tweet table
-        cur.execute("""insert into tweets (tweet_id, tweet_datetime, tweet_keyword, tweet, tweeter, lang, latitude, longitude)
-        select * from temptweets where tweet_id NOT in
-        (select distinct tweet_id from tweets)""")
 
-        # Truncate temp table
-        cur.execute("""truncate temptweets""")
+def copy_tweets():
+    try:
+        saved_tweet_ids = (Tweets
+                           .select(Tweets.tweet_id)
+                           .distinct())
 
-        # Log batch with details
-        cur.execute("""insert into tweetlog (BatchId, keyword, RunDate, HarvestedThisRun, TotalHarvested) values
-        (
-        '"""+str(batch_id)+"""',
-        '"""+str(tweet_keyword)+"""',
-        current_timestamp,
-        ((select count(*) from tweets where tweet_keyword = '"""+str(tweet_keyword)+"""') - (select coalesce(TotalHarvested,0) from tweetlog where keyword = '"""+str(tweet_keyword)+"""' order by RunDate desc limit 1)),
-        (select count(*) from tweets where tweet_keyword = '"""+str(tweet_keyword)+"""')
-        )""")
+        temp_tweets_ids = (Temptweets
+                           .select(Temptweets.tweet_id)
+                           .distinct())
 
-        # Make the changes to the database persistent
-        conn.commit()
+        new_tweet_ids = (temp_tweets_ids - saved_tweet_ids)
 
+        new_tweets = (Temptweets
+                      .select(Temptweets.tweet_id,
+                              Temptweets.tweet_datetime,
+                              Temptweets.tweet_keyword,
+                              Temptweets.tweet,Temptweets.tweeter,
+                              Temptweets.lang, Temptweets.latitude,
+                              Temptweets.longitude)
+                       .where(Temptweets.tweet_id << new_tweet_ids))
+
+        Tweets.insert_from([Tweets.tweet_id, Tweets.tweet_datetime,
+                            Tweets.tweet_keyword, Tweets.tweet,
+                            Tweets.tweeter, Tweets.lang, Tweets.latitude,
+                            Tweets.longitude],new_tweets).execute()
+
+    except:
+        print("~~ Error: {} ~~".format(sys.exc_info()))
+
+    db.execute_sql("truncate table temptweets")
+
+
+#  Bring Calibretto to life
+if __name__ == "__main__":
+    initialize()
+    get_tweets()
