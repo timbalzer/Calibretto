052fa50ae065cb69f921554f49db29ba8ca7904d
Tim Balzer <balzer.tim@gmail.com>
2015-04-19 18:52:12 -0400

    Clean up unnecessary code

--- database_setup.sql
+++ database_setup.sql
@@ -8,39 +8,9 @@ tables needed to run Calibretto
 /*******************************
 Table: tweets
 *******************************/
-
--- DROP TABLE tweets;
-
 CREATE TABLE tweets
 (
-  tweet_id bigint NOT NULL,
-  tweet_datetime timestamp without time zone,
-  tweet_keyword character varying(50),
-  tweet character varying(200),
-  tweeter character varying(50),
-  lang character varying(50),
-  latitude double precision,
-  longitude double precision,
-  CONSTRAINT pk_tweets PRIMARY KEY (tweet_id)
-)
-WITH (
-  OIDS=FALSE
-);
-ALTER TABLE tweets
---  OWNER TO {your username};
-
-
-
-
-/*******************************
-Table: temptweets
-*******************************/
-
--- DROP TABLE temptweets;
-
-CREATE TABLE temptweets
-(
-  tweet_id bigint NOT NULL,
+  tweet_id bigint NOT NULL PRIMARY KEY,
   tweet_datetime timestamp without time zone,
   tweet_keyword character varying(50),
   tweet character varying(200),
@@ -49,12 +19,23 @@ CREATE TABLE temptweets
   latitude double precision,
   longitude double precision
 )
-WITH (
-  OIDS=FALSE
-);
-ALTER TABLE temptweets
---  OWNER TO {your username};
 
+
+
+/*******************************
+Table: temptweets
+*******************************/
+CREATE TABLE temptweets
+(
+  tweet_id bigint NOT NULL PRIMARY KEY,
+  tweet_datetime timestamp without time zone,
+  tweet_keyword character varying(50),
+  tweet character varying(200),
+  tweeter character varying(50),
+  lang character varying(50),
+  latitude double precision,
+  longitude double precision
+)
 
 
 
@@ -63,22 +44,12 @@ ALTER TABLE temptweets
 /*******************************
 Table: tweetlog
 *******************************/
-
--- DROP TABLE tweetlog;
-
 CREATE TABLE tweetlog
 (
-  runid serial NOT NULL,
+  runid serial NOT NULL PRIMARY KEY,
   batchid integer,
   rundate timestamp without time zone,
   keyword character varying(50),
   harvestedthisrun integer,
-  totalharvested integer,
-  CONSTRAINT pk_tweetlog PRIMARY KEY (runid)
+  totalharvested integer
 )
-WITH (
-  OIDS=FALSE
-);
-ALTER TABLE tweetlog
---  OWNER TO {your username};
-
